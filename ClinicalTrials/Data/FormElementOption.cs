228fb96d6143dcdc95d498285f084b61d02e3359
benlagrone <benjaminlagrone@gmail.com>
2015-07-15 15:15:27 -0500

    update objects

--- ClinicalTrials/Data/FormElementOption.cs
+++ ClinicalTrials/Data/FormElementOption.cs
@@ -9,8 +9,8 @@ namespace ClinicalTrials.Data
     {
         public int Id { get; set; }
         public string Label { get; set; }
-        public string Value { get; set; }
-        public string Group { get; set; }
+        public string ValueId { get; set; }
+        public string GroupId { get; set; }
         public DateTime Created { get; set; }
 
         public virtual int? FormElementId { get; set; }
