cb0807968647d26c1926d7ff6de9493837542b84
benlagrone <benjaminlagrone@gmail.com>
2015-07-21 15:31:49 -0500

    adding protocols

--- ClinicalTrials/Controllers/ProtocolFormElementOptionsController.cs
+++ ClinicalTrials/Controllers/ProtocolFormElementOptionsController.cs
@@ -5,22 +5,27 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using ClinicalTrials.Data;
+using System.Web.Http.Cors;
 
 namespace ClinicalTrials.Controllers
 {
+    [EnableCors(origins: "http://gynonc.azurewebsites.net", headers: "*", methods: "*")]
     public class ProtocolFormElementOptionsController : ApiController
     {
          
         private IClinicalTrialsRepository _repo;
+
         public ProtocolFormElementOptionsController(IClinicalTrialsRepository repo)
         {
             _repo = repo;
         }
 
-        //public IEnumerable<ProtocolFormElementOption> Get(int formElementId)
-        //{
-            //return _repo.GetProtocolFormElementOptionsByFormElementId(formElementId);
-        //}
+        public IEnumerable<ProtocolFormElementOption> Get(int formElementOptionId)
+        {
+            var results = _repo.GetProtocolFormElementOptions();
+            var groups = results.Where(t => t.FormElementOption_Id == formElementOptionId);
+            return groups;
+        }
 
     }
 }
